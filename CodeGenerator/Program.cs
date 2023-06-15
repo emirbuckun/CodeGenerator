8652e8509889e9a252d1ebdcd784344a397475cb
Emir Buckun <emirbuckun@hotmail.com>
2023-06-15 17:21:33 +0300

    Code generator program completed

--- CodeGenerator/Program.cs
+++ CodeGenerator/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Declare constants 
+const string AllowedChars = "ACDEFGHKLMNPRTXYZ234579";
+const int Length = 8;
+Random random = new();
+
+// For loop for generating 1000 code examples
+for (int i = 0; i < 1000; i++)
+{
+  char[] code = new char[Length];
+
+  // Get random chars from our allowed chars and put it into code
+  for (int j = 0; j < Length; j++)
+    code[j] = AllowedChars[random.Next(0, AllowedChars.Length)];
+
+  Console.WriteLine(code);
+}
